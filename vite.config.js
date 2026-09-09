4e109cb2eb95335c6e233850355c71d8654f922d
Jonathan Sosa <harlandjp@gmail.com>
2026-09-09 13:45:55 +0900

    Format

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: './',
+  base: "./",
   build: {
     rollupOptions: {
       input: {
-        home: resolve(process.cwd(), 'index.html'),
-        german: resolve(process.cwd(), 'de/index.html'),
-        germanPrivacy: resolve(process.cwd(), 'de/privacy/index.html'),
-        spanish: resolve(process.cwd(), 'es/index.html'),
-        spanishPrivacy: resolve(process.cwd(), 'es/privacy/index.html'),
-        french: resolve(process.cwd(), 'fr/index.html'),
-        frenchPrivacy: resolve(process.cwd(), 'fr/privacy/index.html'),
-        japanese: resolve(process.cwd(), 'ja/index.html'),
-        privacy: resolve(process.cwd(), 'privacy/index.html'),
-        japanesePrivacy: resolve(process.cwd(), 'ja/privacy/index.html'),
+        home: resolve(process.cwd(), "index.html"),
+        german: resolve(process.cwd(), "de/index.html"),
+        germanPrivacy: resolve(process.cwd(), "de/privacy/index.html"),
+        spanish: resolve(process.cwd(), "es/index.html"),
+        spanishPrivacy: resolve(process.cwd(), "es/privacy/index.html"),
+        french: resolve(process.cwd(), "fr/index.html"),
+        frenchPrivacy: resolve(process.cwd(), "fr/privacy/index.html"),
+        japanese: resolve(process.cwd(), "ja/index.html"),
+        privacy: resolve(process.cwd(), "privacy/index.html"),
+        japanesePrivacy: resolve(process.cwd(), "ja/privacy/index.html"),
       },
     },
   },
-})
+});
